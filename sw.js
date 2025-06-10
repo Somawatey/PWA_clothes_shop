3792093386c4289fec5de5897f92818b9596f3a2
Somawatey <148378323+Somawatey@users.noreply.github.com>
2025-06-10 21:46:48 +0700

    v2

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
 const CACHE_NAME = 'stylehub-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/pic.jpg'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/pic.jpg'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(fetchResponse => {
+                    // Cache successful responses
+                    if (fetchResponse && fetchResponse.status === 200) {
+                        const responseToCache = fetchResponse.clone();
+                        caches.open(CACHE_NAME).then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    }
+                    return fetchResponse;
+                });
+            }).catch(() => {
+                // Return cached homepage for navigation requests
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/');
+                }
+            })
+    );
 });
